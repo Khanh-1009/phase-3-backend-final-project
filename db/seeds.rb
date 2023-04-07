2aadb2bd0ef647f054ecc09a0d8242cd04f85322
Khanh Tran <katetrannbkhanh@gmail.com>
2023-04-06 19:28:12 -0700

    Updating

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,28 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
+Brand.create([
+    {
+        name: "Beauty of Joseon"
+    },
+    {
+        name: "Isntree"
+    },
+    {
+        name: "Laneige"
+    },
+    {
+        name: "Skin1004"
+    },
+    {
+        name: "COSRX"
+    },
+    {
+        name: "Frudia"
+    },
+    {
+        name: "Sulwhasoo"
+    }
+])
 
 puts "✅ Done seeding!"
