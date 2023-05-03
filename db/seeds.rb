8d20cecfb8ee219d696f4e7834a6358d833f87a0
Khanh Tran <katetrannbkhanh@gmail.com>
2023-05-02 21:35:21 -0700

    Updating

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,211 +1,211 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
-# Brand.create([
-#     {
-#         name: "Beauty of Joseon"
-#     },
-#     {
-#         name: "Isntree"
-#     },
-#     {
-#         name: "Laneige"
-#     },
-#     {
-#         name: "Skin1004"
-#     },
-#     {
-#         name: "COSRX"
-#     }
-# ])
+Brand.create([
+    {
+        name: "Beauty of Joseon"
+    },
+    {
+        name: "Isntree"
+    },
+    {
+        name: "Laneige"
+    },
+    {
+        name: "Skin1004"
+    },
+    {
+        name: "COSRX"
+    }
+])
 
 Product.create([
-    # {
-    #     name: "Hyaluronic Acid Serum",
-    #     image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/i/s/isntree-hyaluronic-acid-water-essence-50ml-743.jpg",
-    #     price: 18.99,
-    #     brand_id: 2
-    # },
-    # {
-    #     name: "Advanced Snail Cream",
-    #     image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/c/o/cosrx-advanced-snail-92-all-in-one-cream-918.jpg",
-    #     price: 14.91,
-    #     brand_id: 5
-    # },    
-    # {
-    #     name: "Water leeping Mask",
-    #     image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/1000x1231/17f82f742ffe127f42dca9de82fb58b1/l/a/laneige-water-sleeping-mask-ex-70ml-421.jpg",
-    #     price: 20.99,
-    #     brand_id: 3
-    # },
-    # {
-    #     name: "Ginseng Essence Water",
-    #     image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/1000x1231/17f82f742ffe127f42dca9de82fb58b1/b/e/beauty-of-joseon-ginseng-essence-water-150ml-5-fl-oz--512.jpg",
-    #     price: 13.50,
-    #     brand_id: 1
-    # },
-    # {
-    #     name: "Glow Deep Rice Serum",
-    #     image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/b/e/beauty-of-joseon-glow-deep-serum-rice-alpha-arbutin-30ml-708.jpg",
-    #     price: 12.29,
-    #     brand_id: 1
-    # },
-    # {
-    #     name: "Dynasty Cream",
-    #     image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/1000x1231/17f82f742ffe127f42dca9de82fb58b1/b/e/beauty-of-joseon-dynasty-cream-290.jpg",
-    #     price: 18.90,
-    #     brand_id: 1
-    # },
-    # {
-    #     name: "Green Plum Refreshing Toner",
-    #     image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/b/e/beauty-of-joseon-green-plum-refreshing-toner-aha-bha-150ml-410.jpg",
-    #     price: 14.99,
-    #     brand_id: 1
-    # },
-    # {
-    #     name: "Ginseng Cleansing Oil",
-    #     image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/b/e/beauty-of-joseon-ginseng-cleansing-oil-210ml-122.jpg",
-    #     price: 13.29,
-    #     brand_id: 1
-    # },
-    # {
-    #     name: "Hyaluronic Acid Moist Cream",
-    #     image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/1000x1231/17f82f742ffe127f42dca9de82fb58b1/i/s/isntree-hyaluronic-acid-moist-cream-100ml-991.jpg",
-    #     price: 11.99,
-    #     brand_id: 2
-    # },
-    # {
-    #     name: "Green Tea Fresh Toner",
-    #     image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/1000x1231/17f82f742ffe127f42dca9de82fb58b1/i/s/isntree-green-tea-fresh-toner--526.jpg",
-    #     price: 9.99,
-    #     brand_id: 2
-    # },
-    # {
-    #     name: "Green Tea Fresh Cleanser",
-    #     image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/1000x1231/17f82f742ffe127f42dca9de82fb58b1/i/s/isntree-green-tea-fresh-cleanser-120ml-628.jpg",
-    #     price: 11.59,
-    #     brand_id: 2
-    # },
-    # {
-    #     name: "Hyaluronic Acid Toner Plus",
-    #     image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/1000x1231/17f82f742ffe127f42dca9de82fb58b1/i/s/isntree-hyaluronic-acid-toner-plus--637.jpg",
-    #     price: 14.39,
-    #     brand_id: 2
-    # },
-    # {
-    #     name: "Green Tea Fresh Serum",
-    #     image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/1000x1231/17f82f742ffe127f42dca9de82fb58b1/i/s/isntree-green-tea-fresh-serum-50ml-652.jpg",
-    #     price: 15.46,
-    #     brand_id: 2
-    # },
-    # {
-    #     name: "Phyto-Alexin Hydrating & Calming Toner",
-    #     image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/1000x1231/17f82f742ffe127f42dca9de82fb58b1/l/a/laneige-phyto-alexin-hydrating-calming-toner-320ml-775.jpg",
-    #     price: 32.99,
-    #     brand_id: 3
-    # },
-    # {
-    #     name: "Multi Deep Clean Cleanser",
-    #     image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/1000x1231/17f82f742ffe127f42dca9de82fb58b1/l/a/laneige-multi-deep-clean-cleanser-834.jpg",
-    #     price: 21.49,
-    #     brand_id: 3
-    # },
-    # {
-    #     name: "Cica Sleeping Mask",
-    #     image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/1000x1231/17f82f742ffe127f42dca9de82fb58b1/l/a/laneige-cica-sleeping-mask-619.jpg",
-    #     price: 24.99,
-    #     brand_id: 3
-    # },
-    # {
-    #     name: "Perfect Renew Youth Emulsion",
-    #     image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/1000x1231/17f82f742ffe127f42dca9de82fb58b1/l/a/laneige-perfect-renew-youth-emulsion-100ml-237.jpg",
-    #     price: 44.29,
-    #     brand_id: 3
-    # },
-    # {
-    #     name: "Madagascar Centella Ampoule",
-    #     image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/1000x1231/17f82f742ffe127f42dca9de82fb58b1/s/k/skin1004-madagascar-centella-ampoule-100ml-619.jpg",
-    #     price: 14.99,
-    #     brand_id: 4
-    # },
-    # {
-    #     name: "Madagascar Centella Mist",
-    #     image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/1000x1231/17f82f742ffe127f42dca9de82fb58b1/s/k/skin1004-madagascar-centella-hyalu-cica-cloudy-mist-120ml-614.jpg",
-    #     price: 13.49,
-    #     brand_id: 4
-    # },
-    # {
-    #     name: "Madagascar Centella Soothing Cream",
-    #     image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/1000x1231/17f82f742ffe127f42dca9de82fb58b1/s/k/skin1004-madagascar-centella-soothing-cream-75ml-75ml-491.jpg",
-    #     price: 16.89,
-    #     brand_id: 4
-    # },
-    # {
-    #     name: "Madagascar Centella Probio-Cica Toner",
-    #     image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/1000x1231/17f82f742ffe127f42dca9de82fb58b1/s/k/skin1004-madagascar-centella-probio-cica-essense-toner-210ml-624.jpg",
-    #     price: 20.69,
-    #     brand_id: 4
-    # },
-    # {
-    #     name: "Madagascar Centella Poremizing Clear Toner",
-    #     image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/1000x1231/17f82f742ffe127f42dca9de82fb58b1/s/k/skin1004-madagascar-centella-probio-cica-essense-toner-210ml-624.jpg",
-    #     price: 12.99,
-    #     brand_id: 4
-    # },
-    # {
-    #     name: "Morning Gel Cleanser",
-    #     image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/1000x1231/17f82f742ffe127f42dca9de82fb58b1/c/o/cosrx-low-ph-good-morning-gel-cleanser-346.jpg",
-    #     price: 7.91,
-    #     brand_id: 5
-    # },  
-    # {
-    #     name: "Aloe Soothing Sun Cream",
-    #     image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/1000x1231/17f82f742ffe127f42dca9de82fb58b1/c/o/cosrx-aloe-soothing-sun-cream-spf50-pa-50ml-819.jpg",
-    #     price: 12.39,
-    #     brand_id: 5
-    # },  
-    # {
-    #     name: "Full Fit Propolis Synergy Toner",
-    #     image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/1000x1231/17f82f742ffe127f42dca9de82fb58b1/c/o/cosrx-full-fit-propolis-synergy-toner-150ml-561.jpg",
-    #     price: 16.59,
-    #     brand_id: 5
-    # },  
-    # {
-    #     name: "Advanced Snail 96 Mucin Power Essence",
-    #     image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/1000x1231/17f82f742ffe127f42dca9de82fb58b1/c/o/cosrx-advanced-snail-96-mucin-power-essence-100ml-643.jpg",
-    #     price: 12.49,
-    #     brand_id: 5
-    # },  
-    # {
-    #     name: "Concentrated Ginseng Renewing Emulsion",
-    #     image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/1000x1231/17f82f742ffe127f42dca9de82fb58b1/s/u/sulwhasoo-concentrated-ginseng-renewing-emulsion-125ml-564.jpg",
-    #     price: 74.69,
-    #     brand_id: 19
-    # },
-    # {
-    #     name: "Essential Perfecting Firming Cream",
-    #     image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/1000x1231/17f82f742ffe127f42dca9de82fb58b1/s/u/sulwhasoo-essential-perfecting-firming-cream-75ml-188.jpg",
-    #     price: 89.99,
-    #     brand_id: 19
-    # },
-    # {
-    #     name: "Gentle Cleansing Oil Makeup Remover",
-    #     image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/1000x1231/17f82f742ffe127f42dca9de82fb58b1/s/u/sulwhasoo-gentle-cleansing-oil-200ml-534.jpg",
-    #     price: 30.99,
-    #     brand_id: 19
-    # },
-    # {
-    #     name: "Timetreasure Invigorating Emulsion",
-    #     image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/1000x1231/17f82f742ffe127f42dca9de82fb58b1/b/s/bsemsul00003_par.jpg",
-    #     price: 82.29,
-    #     brand_id: 19
-    # },
-    # {
-    #     name: "Timetreasure Extra Creamy Cleansing Foam",
-    #     image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/1000x1231/17f82f742ffe127f42dca9de82fb58b1/s/u/sulwhasoo-timetreasure-extra-creamy-cleansing-foam-150ml-305.jpg",
-    #     price: 39.69,
-    #     brand_id: 19
-    # }
+    {
+        name: "Hyaluronic Acid Serum",
+        image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/i/s/isntree-hyaluronic-acid-water-essence-50ml-743.jpg",
+        price: 18.99,
+        brand_id: 2
+    },
+    {
+        name: "Advanced Snail Cream",
+        image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/c/o/cosrx-advanced-snail-92-all-in-one-cream-918.jpg",
+        price: 14.91,
+        brand_id: 5
+    },    
+    {
+        name: "Water leeping Mask",
+        image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/1000x1231/17f82f742ffe127f42dca9de82fb58b1/l/a/laneige-water-sleeping-mask-ex-70ml-421.jpg",
+        price: 20.99,
+        brand_id: 3
+    },
+    {
+        name: "Ginseng Essence Water",
+        image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/1000x1231/17f82f742ffe127f42dca9de82fb58b1/b/e/beauty-of-joseon-ginseng-essence-water-150ml-5-fl-oz--512.jpg",
+        price: 13.50,
+        brand_id: 1
+    },
+    {
+        name: "Glow Deep Rice Serum",
+        image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/b/e/beauty-of-joseon-glow-deep-serum-rice-alpha-arbutin-30ml-708.jpg",
+        price: 12.29,
+        brand_id: 1
+    },
+    {
+        name: "Dynasty Cream",
+        image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/1000x1231/17f82f742ffe127f42dca9de82fb58b1/b/e/beauty-of-joseon-dynasty-cream-290.jpg",
+        price: 18.90,
+        brand_id: 1
+    },
+    {
+        name: "Green Plum Refreshing Toner",
+        image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/b/e/beauty-of-joseon-green-plum-refreshing-toner-aha-bha-150ml-410.jpg",
+        price: 14.99,
+        brand_id: 1
+    },
+    {
+        name: "Ginseng Cleansing Oil",
+        image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/b/e/beauty-of-joseon-ginseng-cleansing-oil-210ml-122.jpg",
+        price: 13.29,
+        brand_id: 1
+    },
+    {
+        name: "Hyaluronic Acid Moist Cream",
+        image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/1000x1231/17f82f742ffe127f42dca9de82fb58b1/i/s/isntree-hyaluronic-acid-moist-cream-100ml-991.jpg",
+        price: 11.99,
+        brand_id: 2
+    },
+    {
+        name: "Green Tea Fresh Toner",
+        image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/1000x1231/17f82f742ffe127f42dca9de82fb58b1/i/s/isntree-green-tea-fresh-toner--526.jpg",
+        price: 9.99,
+        brand_id: 2
+    },
+    {
+        name: "Green Tea Fresh Cleanser",
+        image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/1000x1231/17f82f742ffe127f42dca9de82fb58b1/i/s/isntree-green-tea-fresh-cleanser-120ml-628.jpg",
+        price: 11.59,
+        brand_id: 2
+    },
+    {
+        name: "Hyaluronic Acid Toner Plus",
+        image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/1000x1231/17f82f742ffe127f42dca9de82fb58b1/i/s/isntree-hyaluronic-acid-toner-plus--637.jpg",
+        price: 14.39,
+        brand_id: 2
+    },
+    {
+        name: "Green Tea Fresh Serum",
+        image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/1000x1231/17f82f742ffe127f42dca9de82fb58b1/i/s/isntree-green-tea-fresh-serum-50ml-652.jpg",
+        price: 15.46,
+        brand_id: 2
+    },
+    {
+        name: "Phyto-Alexin Hydrating & Calming Toner",
+        image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/1000x1231/17f82f742ffe127f42dca9de82fb58b1/l/a/laneige-phyto-alexin-hydrating-calming-toner-320ml-775.jpg",
+        price: 32.99,
+        brand_id: 3
+    },
+    {
+        name: "Multi Deep Clean Cleanser",
+        image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/1000x1231/17f82f742ffe127f42dca9de82fb58b1/l/a/laneige-multi-deep-clean-cleanser-834.jpg",
+        price: 21.49,
+        brand_id: 3
+    },
+    {
+        name: "Cica Sleeping Mask",
+        image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/1000x1231/17f82f742ffe127f42dca9de82fb58b1/l/a/laneige-cica-sleeping-mask-619.jpg",
+        price: 24.99,
+        brand_id: 3
+    },
+    {
+        name: "Perfect Renew Youth Emulsion",
+        image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/1000x1231/17f82f742ffe127f42dca9de82fb58b1/l/a/laneige-perfect-renew-youth-emulsion-100ml-237.jpg",
+        price: 44.29,
+        brand_id: 3
+    },
+    {
+        name: "Madagascar Centella Ampoule",
+        image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/1000x1231/17f82f742ffe127f42dca9de82fb58b1/s/k/skin1004-madagascar-centella-ampoule-100ml-619.jpg",
+        price: 14.99,
+        brand_id: 4
+    },
+    {
+        name: "Madagascar Centella Mist",
+        image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/1000x1231/17f82f742ffe127f42dca9de82fb58b1/s/k/skin1004-madagascar-centella-hyalu-cica-cloudy-mist-120ml-614.jpg",
+        price: 13.49,
+        brand_id: 4
+    },
+    {
+        name: "Madagascar Centella Soothing Cream",
+        image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/1000x1231/17f82f742ffe127f42dca9de82fb58b1/s/k/skin1004-madagascar-centella-soothing-cream-75ml-75ml-491.jpg",
+        price: 16.89,
+        brand_id: 4
+    },
+    {
+        name: "Madagascar Centella Probio-Cica Toner",
+        image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/1000x1231/17f82f742ffe127f42dca9de82fb58b1/s/k/skin1004-madagascar-centella-probio-cica-essense-toner-210ml-624.jpg",
+        price: 20.69,
+        brand_id: 4
+    },
+    {
+        name: "Madagascar Centella Poremizing Clear Toner",
+        image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/1000x1231/17f82f742ffe127f42dca9de82fb58b1/s/k/skin1004-madagascar-centella-probio-cica-essense-toner-210ml-624.jpg",
+        price: 12.99,
+        brand_id: 4
+    },
+    {
+        name: "Morning Gel Cleanser",
+        image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/1000x1231/17f82f742ffe127f42dca9de82fb58b1/c/o/cosrx-low-ph-good-morning-gel-cleanser-346.jpg",
+        price: 7.91,
+        brand_id: 5
+    },  
+    {
+        name: "Aloe Soothing Sun Cream",
+        image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/1000x1231/17f82f742ffe127f42dca9de82fb58b1/c/o/cosrx-aloe-soothing-sun-cream-spf50-pa-50ml-819.jpg",
+        price: 12.39,
+        brand_id: 5
+    },  
+    {
+        name: "Full Fit Propolis Synergy Toner",
+        image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/1000x1231/17f82f742ffe127f42dca9de82fb58b1/c/o/cosrx-full-fit-propolis-synergy-toner-150ml-561.jpg",
+        price: 16.59,
+        brand_id: 5
+    },  
+    {
+        name: "Advanced Snail 96 Mucin Power Essence",
+        image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/1000x1231/17f82f742ffe127f42dca9de82fb58b1/c/o/cosrx-advanced-snail-96-mucin-power-essence-100ml-643.jpg",
+        price: 12.49,
+        brand_id: 5
+    },  
+    {
+        name: "Concentrated Ginseng Renewing Emulsion",
+        image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/1000x1231/17f82f742ffe127f42dca9de82fb58b1/s/u/sulwhasoo-concentrated-ginseng-renewing-emulsion-125ml-564.jpg",
+        price: 74.69,
+        brand_id: 19
+    },
+    {
+        name: "Essential Perfecting Firming Cream",
+        image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/1000x1231/17f82f742ffe127f42dca9de82fb58b1/s/u/sulwhasoo-essential-perfecting-firming-cream-75ml-188.jpg",
+        price: 89.99,
+        brand_id: 19
+    },
+    {
+        name: "Gentle Cleansing Oil Makeup Remover",
+        image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/1000x1231/17f82f742ffe127f42dca9de82fb58b1/s/u/sulwhasoo-gentle-cleansing-oil-200ml-534.jpg",
+        price: 30.99,
+        brand_id: 19
+    },
+    {
+        name: "Timetreasure Invigorating Emulsion",
+        image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/1000x1231/17f82f742ffe127f42dca9de82fb58b1/b/s/bsemsul00003_par.jpg",
+        price: 82.29,
+        brand_id: 19
+    },
+    {
+        name: "Timetreasure Extra Creamy Cleansing Foam",
+        image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/1000x1231/17f82f742ffe127f42dca9de82fb58b1/s/u/sulwhasoo-timetreasure-extra-creamy-cleansing-foam-150ml-305.jpg",
+        price: 39.69,
+        brand_id: 19
+    },
     {
         name: "Madagascar Centella Poremizing Light Gel Cream",
         image: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/1000x1231/17f82f742ffe127f42dca9de82fb58b1/s/k/skin1004-madagascar-centella-poremizing-light-gel-cream-75ml-255.jpg",
